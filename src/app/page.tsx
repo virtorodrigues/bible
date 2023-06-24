d8cdd4f946d3d1ee46354094e2e137856cc1d3f7
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-24 17:53:40 -0300

    init the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { RandomVerse } from "@/components/RandomVerse"
+
+
+export default function Home() {
+
+  return (
+    <div className='flex flex-col justify-start p-10'>
+      <RandomVerse />
+    </div>
+  )
+}
---
+++ src/components/RandomVerse.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import { data } from "autoprefixer"
+import { useEffect, useState } from "react"
+
+interface DataInterface {
+  book: {
+    name: string
+  }
+  chapter: string
+  number: number
+  text: string
+}
+
+export function RandomVerse() {
+  const [verse, setVerse] = useState<DataInterface | null>({} as DataInterface)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    generateRamdonVerse()
+  }, [])
+
+  async function generateRamdonVerse() {
+    const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlNhdCBKdW4gMjQgMjAyMyAxOTo0MDozMiBHTVQrMDAwMC42NDk3NDMwOTcyN2FlOTAwMjA0MDg4YTYiLCJpYXQiOjE2ODc2MzU2MzJ9.0hEiCwoBQHvhglSTxLPWSFIMRaTYhz4lWHEzeaO3BcI'
+
+    setIsLoading(true)
+    setVerse(null)
+
+    const bible = await fetch('https://www.abibliadigital.com.br/api/verses/nvi/random', {
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    })
+
+    setIsLoading(false)
+
+    const data = await bible.json()
+
+    const informationsRequests = await fetch('https://www.abibliadigital.com.br/api/requests/amount/day', {
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    })
+    const informationsRequestsData = await informationsRequests.json()
+    console.log(informationsRequestsData)
+
+    setVerse(data)
+  }
+
+  console.log('dasdas')
+
+  return (
+    <>
+      {isLoading || !verse ? (
+        <span>carregando...</span>
+      ) : (
+          <>
+            <div className='gap-2 flex'>
+              <span>{verse?.book?.name}</span>
+              <span>capítulo: {verse?.chapter}</span>
+              <span>versículo: {verse?.number}</span>
+            </div>
+            <span className='mt-5 first-letter:uppercase'>{verse?.text}</span>
+          </>
+
+        )
+      }
+
+      <button disabled={isLoading || !verse} onClick={generateRamdonVerse} className='w-fit mt-10 bg-gray-600 px-3 py-1 rounded hover:bg-gray-700'>Gerar nova</button>
+
+    </>
+  )
+}
+
+
+/*
+
+    /*const createUser = await fetch('https://www.abibliadigital.com.br/api/users', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "name": "vitor",
+      "email": "santos_vitao@hotmail.com",
+      "password": "123123123", // minimum size 6 digits
+      "notifications": true // receive update emails from www.abibliadigital.com.br
+    })
+  })
+  const createUSerData = await createUser.json()
+  console.log(createUSerData)
+  */
+  /*const token = await fetch('https://www.abibliadigital.com.br/api/users/token', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT',
+    body: JSON.stringify({
+      "email": "santos_vitao@hotmail.com",
+      "password": "123123123",
+    })
+  })
+  const tokenData = await token.json()
+  console.log(tokenData.token)
+*/
+
+/* const userByEmail = await fetch('https://www.abibliadigital.com.br/api/users/santos_vitao@hotmail.com', {
+    headers:{
+      Authorization: `Bearer ${TOKEN}`
+    }
+  })
+  const data = await userByEmail.json()
+  console.log(data)
+*/
