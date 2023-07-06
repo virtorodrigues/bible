9242592350ccc21c41da6394a89aa23ec1c2b8fc
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-07-05 22:34:31 -0300

    feat: add email validation to send the email and add in firebase

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -1,36 +1,43 @@
 'use client'
 
-import { ChangeEvent, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function Subscription() {
-  const [email, setEmail] = useState('')
+  const emailSchema = z.object({
+    email: z
+      .string()
+      .min(3, { message: 'Insira um e-mail válido' })
+      .email('Insira um e-mail válido'),
+  })
+
+  type EmailType = z.infer<typeof emailSchema>
 
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm()
+  } = useForm<EmailType>({
+    resolver: zodResolver(emailSchema),
+  })
 
-  async function registerUser() {
-    if (email.length > 3) {
-      const informationsRequests = await fetch(
-        `${process.env.NEXT_PUBLIC_URL}api/email/create-user`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email,
-            name,
-            html: getHTMLToEmail({ email }),
-          }),
-        },
-      )
+  async function registerUser(data: { email: string }) {
+    console.log()
+    const informationsRequests = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}api/email/create-user`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: data.email,
+          name,
+          html: getHTMLToEmail({ email: data.email }),
+        }),
+      },
+    )
 
-      const information = await informationsRequests.json()
-      console.log(information)
-      setEmail('')
-    }
+    const information = await informationsRequests.json()
+    console.log(information)
   }
 
   function getHTMLToEmail({ email }: { email: string }) {
@@ -52,21 +59,27 @@ export function Subscription() {
       </h2>
       <form
         onSubmit={handleSubmit(registerUser)}
-        className="mb-1 flex h-10 w-full flex-row items-start justify-center gap-4 md:h-10 md:flex-row"
+        className="mb-1 flex w-full flex-col items-start justify-center gap-2"
       >
-        <input
-          type="text"
-          placeholder="E-mail"
-          className="h-full w-full rounded border border-gray-400 px-3 text-sm text-black"
-          value={email}
-          {...register('email')}
-        />
-        <button
-          type="submit"
-          className="h-full cursor-pointer rounded bg-white px-4 text-sm text-gray-700 duration-200 hover:box-shadow-lg md:px-5"
-        >
-          Cadastrar
-        </button>
+        <div className="flex h-10 w-full flex-row items-start justify-center gap-4 md:h-10 md:flex-row">
+          <div className="flex h-full w-full flex-col">
+            <input
+              type="text"
+              placeholder="E-mail"
+              className="h-full w-full rounded border border-gray-400 px-3 text-sm text-black"
+              {...register('email')}
+            />
+          </div>
+          <button
+            type="submit"
+            className="h-full cursor-pointer rounded bg-white px-4 text-sm text-gray-700 duration-200 hover:box-shadow-lg md:px-5"
+          >
+            Cadastrar
+          </button>
+        </div>
+        {errors.email && (
+          <span className="text-sm text-red-500">{errors.email?.message}</span>
+        )}
       </form>
     </div>
   )
